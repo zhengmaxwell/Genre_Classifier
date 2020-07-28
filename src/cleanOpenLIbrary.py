a2230857aa7fbc352f654cba358604b066f70dc8
Maxwell Zheng <39140008+zhengmaxwell@users.noreply.github.com>
2020-07-27 20:49:17 -0400

    Add files via upload

---
+++ src/cleanOpenLIbrary.py
@@ -0,0 +1,55 @@
+import csv
+import re
+import string
+from typing import List, Dict
+
+
+
+def _cleanData(title: str, author: str, description: str) -> str:
+
+    escapes = ''.join([chr(char) for char in range(1, 32)])
+    translator = str.maketrans('', '', escapes)
+    printable = set(string.printable)
+
+    badWords = f"{author}|isbn|{title}|new york times|bestseller"
+
+    cleanString = ''.join(filter(lambda x: x in printable, re.sub('<[^<]+?>', '', description.translate(translator)))) # data cleaning HTML tags and non-ASCII 128 chars
+    cleanString = cleanString.translate(str.maketrans(' ',' ', string.punctuation))
+
+    cleanDescription = []
+
+    for sentence in cleanString.split("."): # removes sentences containing bad words
+        if not (re.search(badWords, sentence, flags=re.IGNORECASE)):
+            cleanDescription.append(sentence.lower())
+
+    cleanDescription = " ".join(cleanDescription)
+
+    return cleanDescription
+
+
+def toCSV(data: List[Dict[str, str]]) -> None:
+
+    keys = data[0].keys()
+
+    outputFile = open(r"openLibraryData.csv", 'w', newline='', encoding="utf-8")
+
+    writer = csv.DictWriter(outputFile, keys)
+    writer.writeheader()
+    writer.writerows(data) 
+
+
+if __name__ == "__main__":
+
+    reader = csv.DictReader(open(r"C:\Users\mazheng\Downloads\openLIbraryData.csv", encoding="utf8"))
+
+    cleanedData = []
+
+    for row in reader:
+        description = _cleanData(row["title"], row["author"], row["description"])
+        if description:
+            book = {"description": description, "genre": row["genre"]}
+            cleanedData.append(book)
+
+    toCSV(cleanedData)
+
+
